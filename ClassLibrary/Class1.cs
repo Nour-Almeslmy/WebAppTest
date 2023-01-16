cde0c0cca6f652c62e8257170364b7c217263fbf
Nour Almelsmy <91690116+Nour-Almeslmy@users.noreply.github.com>
2023-01-16 11:36:28 +0200

    add class librrary

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public string Value { get; set; } = "VALUE";
+    }
+}
---
+++ ProjTestDep/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjTestDep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ActionResult<string> GetValue()
+        {
+            var class1 = new Class1();
+            return Ok(class1.Value);
+        }
+    }
+}
